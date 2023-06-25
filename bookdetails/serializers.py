fa9bf4791a116c45b4e7a1d72c8a34dca35ce3f8
irisru398 <irisru309@gmail.com>
2023-06-25 12:53:29 -0400

    update bookdetails

--- bookdetails/serializers.py
+++ bookdetails/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
 from .models import Book
+from .models import Author
 
+#author serializer has to be created to handle author models
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'
+        
+#book serializer has to be created to handle book models
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
+
--- bookdetails/urls.py
+++ bookdetails/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from .views import BookListView #view that allows user to view all books within db
 from .views import BookCreateView #view that will allow user to input new books
 from .views import BookInfoView #view that will allow user to get all book by isbn within db
+from .views import AuthorCreateView #view that will allow an admin to create a new author within db
 
 app_name = 'bookdetails'
 
@@ -26,6 +27,7 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/create/', BookCreateView.as_view(), name='create_book'),
     path('books/<str:isbn>/', BookInfoView.as_view(), name='detail-book'),
+    path('author/create', AuthorCreateView.as_view(), name='create_author')
 ]
 
 
